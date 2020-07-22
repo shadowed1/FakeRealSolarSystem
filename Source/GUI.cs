d566810d452574a891c42d458b2de847ed498e14
siimav <siim.aaver@gmail.com>
2020-07-22 19:10:42 +0300

    Add debug GUI

--- Source/GUI.cs
+++ Source/GUI.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Kopernicus.Configuration.ModLoader;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace RealSolarSystem
@@ -20,6 +23,39 @@ namespace RealSolarSystem
         // Camera parameters.
 
         List<CameraWrapper> cams = null;
+
+        private string sMinDist = null;
+        private string sMinDiv = null;
+        private string sMaxDiv = null;
+
+        private string sDepthOffset = null;
+        private string sOceanRadiusOffset = null;
+        private string sCoastOrder = null;
+
+        private string sOceanFactor = null;
+        private string sCoastLessThan = null;
+        private string sCoastFactor = null;
+        private string sEnhanceOrder = null;
+
+        private string sMinHeightOffset;
+        private string sMaxHeightOffset;
+        private string sSlopeScale;
+        private string sRssDefineOrder;
+
+        private string sHeightStart;
+        private string sHeightEnd;
+        private string sDeformity;
+        private string sFrequency;
+        private string sOctaves;
+        private string sPersistance;
+        private string sHeightNoiseOrder;
+
+        List<PQSMod> modList;
+        PQSMod_VertexDefineCoastLine pModDefine = null;
+        PQSMod_QuadEnhanceCoast pModEnhance = null;
+        PQSMod_VertexDefineCoastSmooth pModRssDefine = null;
+        PQSMod_VertexHeightNoiseVertHeight pModHeightNoise = null;
+        PQSMod_VertexHeightMapRSS pModRssHMap = null;
 
         public class CameraWrapper : MonoBehaviour
         {
@@ -115,6 +151,11 @@ namespace RealSolarSystem
             {
                 GUIOpen = !GUIOpen;
             }
+
+            if (Input.GetKeyDown(KeyCode.R) && Input.GetKey(KeyCode.LeftAlt))
+            {
+                FlightGlobals.currentMainBody?.pqsController?.StartUpSphere();
+            }
         }
 
         public void OnGUI()
@@ -190,9 +231,312 @@ namespace RealSolarSystem
                 }
             }
 
+            var pqs = FlightGlobals.currentMainBody.pqsController;
+            if (modList == null)
+            {
+                modList = new List<PQSMod>();
+                GetChildMods(pqs.gameObject, modList);
+                pModDefine = (PQSMod_VertexDefineCoastLine)modList.FirstOrDefault(t => t is PQSMod_VertexDefineCoastLine);
+                pModEnhance = (PQSMod_QuadEnhanceCoast)modList.FirstOrDefault(t => t is PQSMod_QuadEnhanceCoast);
+                pModRssDefine = (PQSMod_VertexDefineCoastSmooth)modList.FirstOrDefault(t => t is PQSMod_VertexDefineCoastSmooth);
+                pModHeightNoise = (PQSMod_VertexHeightNoiseVertHeight)modList.FirstOrDefault(t => t is PQSMod_VertexHeightNoiseVertHeight);
+                pModRssHMap = (PQSMod_VertexHeightMapRSS)modList.FirstOrDefault(t => t is PQSMod_VertexHeightMapRSS);
+            }
+
+            PQSCache.PQSSpherePreset preset = PQSCache.PresetList?.GetPreset(pqs.gameObject.name);
+            if (preset != null)
+            {
+                if (sMinDist == null)
+                {
+                    sMinDist = preset.minDistance.ToString();
+                    sMinDiv = preset.minSubdivision.ToString();
+                    sMaxDiv = preset.maxSubdivision.ToString();
+
+                    if (pModDefine != null)
+                    {
+                        sDepthOffset = pModDefine.depthOffset.ToString();
+                        sOceanRadiusOffset = pModDefine.oceanRadiusOffset.ToString();
+                        sCoastOrder = pModDefine.order.ToString();
+                    }
+                    if (pModEnhance != null)
+                    {
+                        sOceanFactor = pModEnhance.oceanFactor.ToString();
+                        sCoastLessThan = pModEnhance.coastLessThan.ToString();
+                        sCoastFactor = pModEnhance.coastFactor.ToString();
+                        sEnhanceOrder = pModEnhance.order.ToString();
+                    }
+                    if (pModRssDefine != null)
+                    {
+                        sMinHeightOffset = pModRssDefine.minHeightOffset.ToString();
+                        sMaxHeightOffset = pModRssDefine.maxHeightOffset.ToString();
+                        sSlopeScale = pModRssDefine.slopeScale.ToString();
+                        sRssDefineOrder = pModRssDefine.order.ToString();
+                    }
+                    if (pModHeightNoise != null)
+                    {
+                        sHeightStart = pModHeightNoise.heightStart.ToString();
+                        sHeightEnd = pModHeightNoise.heightEnd.ToString();
+                        sDeformity = pModHeightNoise.deformity.ToString();
+                        sFrequency = pModHeightNoise.frequency.ToString();
+                        sOctaves = pModHeightNoise.octaves.ToString();
+                        sPersistance = pModHeightNoise.persistance.ToString();
+                        // TODO: other fields
+                        sHeightNoiseOrder = pModHeightNoise.order.ToString();
+                    }
+                }
+
+                GUILayout.Label("Preset " + preset.name);
+
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("minDistance: ");
+                GUILayout.EndHorizontal();
+                sMinDist = GUILayout.TextField(sMinDist);
+                if (double.TryParse(sMinDist, out double minDist))
+                {
+                    preset.minDistance = minDist;
+                }
+
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("minSubdivision: ");
+                GUILayout.EndHorizontal();
+                sMinDiv = GUILayout.TextField(sMinDiv);
+                if (int.TryParse(sMinDiv, out int minDiv))
+                {
+                    preset.minSubdivision = minDiv;
+                }
+
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("maxSubdivision: ");
+                GUILayout.EndHorizontal();
+                sMaxDiv = GUILayout.TextField(sMaxDiv);
+                if (int.TryParse(sMaxDiv, out int maxDiv))
+                {
+                    preset.maxSubdivision = maxDiv;
+                }
+
+                GUILayout.Label("-----------------");
+                foreach (PQSMod pqsmod in modList)
+                {
+                    GUILayout.BeginHorizontal();
+                    pqsmod.modEnabled = GUILayout.Toggle(pqsmod.modEnabled, pqsmod.GetType().Name);
+                    GUILayout.EndHorizontal();
+                }
+
+                if (pModDefine != null)
+                {
+                    GUILayout.Label("-----------------");
+                    GUILayout.Label("VertexDefineCoastLine");
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Coastline depthOffset: ");
+                    GUILayout.EndHorizontal();
+                    sDepthOffset = GUILayout.TextField(sDepthOffset);
+                    if (double.TryParse(sDepthOffset, out double depthOffset))
+                    {
+                        pModDefine.depthOffset = depthOffset;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Coastline oceanRadiusOffset: ");
+                    GUILayout.EndHorizontal();
+                    sOceanRadiusOffset = GUILayout.TextField(sOceanRadiusOffset);
+                    if (double.TryParse(sOceanRadiusOffset, out double oceanRadiusOffset))
+                    {
+                        pModDefine.oceanRadiusOffset = oceanRadiusOffset;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Coastline order: ");
+                    GUILayout.EndHorizontal();
+                    sCoastOrder = GUILayout.TextField(sCoastOrder);
+                    if (int.TryParse(sCoastOrder, out int coastOrder))
+                    {
+                        pModDefine.order = coastOrder;
+                    }
+                }
+
+                if (pModEnhance!= null)
+                {
+                    GUILayout.Label("-----------------");
+                    GUILayout.Label("QuadEnhanceCoast");
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Enhance oceanFactor: ");
+                    GUILayout.EndHorizontal();
+                    sOceanFactor = GUILayout.TextField(sOceanFactor);
+                    if (double.TryParse(sOceanFactor, out double oceanfactor))
+                    {
+                        pModEnhance.oceanFactor = oceanfactor;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Enhance coastFactor: ");
+                    GUILayout.EndHorizontal();
+                    sCoastFactor = GUILayout.TextField(sCoastFactor);
+                    if (double.TryParse(sCoastFactor, out double coastFactor))
+                    {
+                        pModEnhance.coastFactor = coastFactor;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Enhance coastLessThan: ");
+                    GUILayout.EndHorizontal();
+                    sCoastLessThan = GUILayout.TextField(sCoastLessThan);
+                    if (double.TryParse(sCoastLessThan, out double lt))
+                    {
+                        pModEnhance.coastLessThan = lt;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Enhance order: ");
+                    GUILayout.EndHorizontal();
+                    sEnhanceOrder = GUILayout.TextField(sEnhanceOrder);
+                    if (int.TryParse(sEnhanceOrder, out int order))
+                    {
+                        pModEnhance.order = order;
+                    }
+                }
+
+                if (pModRssDefine != null)
+                {
+                    GUILayout.Label("-----------------");
+                    GUILayout.Label("VertexDefineCoastSmooth");
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("minHeightOffset: ");
+                    GUILayout.EndHorizontal();
+                    sMinHeightOffset = GUILayout.TextField(sMinHeightOffset);
+                    if (double.TryParse(sMinHeightOffset, out double minHeightOffset))
+                    {
+                        pModRssDefine.minHeightOffset = minHeightOffset;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("maxHeightOffset: ");
+                    GUILayout.EndHorizontal();
+                    sMaxHeightOffset = GUILayout.TextField(sMaxHeightOffset);
+                    if (double.TryParse(sMaxHeightOffset, out double maxHeightOffset))
+                    {
+                        pModRssDefine.maxHeightOffset = maxHeightOffset;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("slopeScale: ");
+                    GUILayout.EndHorizontal();
+                    sSlopeScale = GUILayout.TextField(sSlopeScale);
+                    if (double.TryParse(sSlopeScale, out double val))
+                    {
+                        pModRssDefine.slopeScale = val;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Mod order: ");
+                    GUILayout.EndHorizontal();
+                    sRssDefineOrder = GUILayout.TextField(sRssDefineOrder);
+                    if (int.TryParse(sRssDefineOrder, out int order))
+                    {
+                        pModRssDefine.order = order;
+                    }
+                }
+
+                if (pModHeightNoise != null)
+                {
+                    GUILayout.Label("-----------------");
+                    GUILayout.Label("VertexHeightNoiseVertHeight");
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("heightStart: ");
+                    GUILayout.EndHorizontal();
+                    sHeightStart = GUILayout.TextField(sHeightStart);
+                    if (double.TryParse(sHeightStart, out double val))
+                    {
+                        pModHeightNoise.heightStart = (float)val;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("maxHeightOffset: ");
+                    GUILayout.EndHorizontal();
+                    sHeightEnd = GUILayout.TextField(sHeightEnd);
+                    if (double.TryParse(sHeightEnd, out val))
+                    {
+                        pModHeightNoise.heightEnd = (float)val;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("deformity: ");
+                    GUILayout.EndHorizontal();
+                    sDeformity = GUILayout.TextField(sDeformity);
+                    if (double.TryParse(sDeformity, out val))
+                    {
+                        pModHeightNoise.deformity = (float)val;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("frequency: ");
+                    GUILayout.EndHorizontal();
+                    sFrequency = GUILayout.TextField(sFrequency);
+                    if (double.TryParse(sFrequency, out val))
+                    {
+                        pModHeightNoise.frequency = (float)val;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("octaves: ");
+                    GUILayout.EndHorizontal();
+                    sOctaves = GUILayout.TextField(sOctaves);
+                    if (int.TryParse(sOctaves, out int val2))
+                    {
+                        pModHeightNoise.octaves = val2;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("persistance: ");
+                    GUILayout.EndHorizontal();
+                    sPersistance = GUILayout.TextField(sPersistance);
+                    if (double.TryParse(sPersistance, out val))
+                    {
+                        pModHeightNoise.persistance = (float)val;
+                    }
+
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Label("Mod order: ");
+                    GUILayout.EndHorizontal();
+                    sHeightNoiseOrder = GUILayout.TextField(sHeightNoiseOrder);
+                    if (int.TryParse(sHeightNoiseOrder, out int order))
+                    {
+                        pModHeightNoise.order = order;
+                    }
+                }
+            }
+
             GUILayout.EndScrollView();
             GUILayout.EndVertical();
             GUI.DragWindow();
+        }
+
+        private void GetChildMods(GameObject obj, List<PQSMod> mods)
+        {
+            IEnumerator enumerator = obj.transform.GetEnumerator();
+            try
+            {
+                while (enumerator.MoveNext())
+                {
+                    Transform current = (Transform)enumerator.Current;
+                    if (!(current == transform) && !(current.GetComponent<PQS>() != null))
+                    {
+                        PQSMod[] components = current.GetComponents<PQSMod>();
+                        if (components != null)
+                        {
+                            mods.AddRange(components);
+                            GetChildMods(current.gameObject, mods);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                (enumerator as IDisposable)?.Dispose();
+            }
         }
     }
 }
